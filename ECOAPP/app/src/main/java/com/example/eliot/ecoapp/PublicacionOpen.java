fa601801a4cf11a9721746bd7a944744b4b13a01
eliotgnr11 <eliotgnr11@gmail.com>
2018-04-20 11:57:33 -0500

    S2
    Diseno de pantallas en android studio

---
+++ ECOAPP/app/src/main/java/com/example/eliot/ecoapp/PublicacionOpen.java
@@ -0,0 +1,13 @@
+package com.example.eliot.ecoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PublicacionOpen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicacion_open);
+    }
+}
