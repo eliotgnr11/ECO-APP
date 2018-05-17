c39718b414a7610d2facf9f34d4f5d69ae628cfe
eliotgnr11 <eliotgnr11@gmail.com>
2018-05-17 11:08:30 -0500

    S3
    ventanas y botones

--- ECOAPP/app/src/main/java/com/example/eliot/ecoapp/MenuPrincipal.java
+++ ECOAPP/app/src/main/java/com/example/eliot/ecoapp/MenuPrincipal.java
@@ -1,9 +1,8 @@
 package com.example.eliot.ecoapp;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,17 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuPrincipal extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
 
+        implements NavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -32,6 +33,57 @@ public class MenuPrincipal extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ImageButton btnsocial= (ImageButton) findViewById(R.id.btnsocial);
+        btnsocial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Muro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        ImageButton btntips= (ImageButton) findViewById(R.id.btntips);
+        btntips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Muro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        ImageButton btntendencias= (ImageButton) findViewById(R.id.btntendencias);
+        btntendencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Muro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        ImageButton btnnoticias= (ImageButton) findViewById(R.id.btnnoticias);
+        btnnoticias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Muro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+
+        ImageButton btncompras= (ImageButton) findViewById(R.id.btncompras);
+        btncompras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Muro.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 
     @Override
@@ -69,21 +121,25 @@ public class MenuPrincipal extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_ayuda) {
+            Intent intent = new Intent(MenuPrincipal.this, Ayuda.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_acerca) {
+            Intent intent = new Intent(MenuPrincipal.this, AcercaDe.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_perfil) {
+            Intent intent = new Intent(MenuPrincipal.this, Perfil.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_salir) {
+            System.exit(0);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
